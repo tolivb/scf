d0c2b818cc46bf8c995116efc4afd17a7c9a6d70
ToliB <avb@ucdn.com>
2017-02-11 02:56:24 +0200

    Add better log messages when creating syslog service

--- pkg/services/services.go
+++ pkg/services/services.go
@@ -21,36 +21,37 @@ type Syslog struct {
 func (s *Syslog) Start() error {
 
 	if s.conn != nil {
-		return fmt.Errorf("Service already started on addr  %s", s.addr)
+		return fmt.Errorf("Syslog service already started on %s", s.addr)
 	}
 
 	udpAddr, err := net.ResolveUDPAddr(s.net, s.addr)
-	s.cfg.Log.Debug("Resolved syslog addr %", udpAddr)
 
 	if err != nil {
-		return err
+		return fmt.Errorf("Syslog unable to resolve listen addr: %s", s.addr, err)
 	}
+
+	s.cfg.Log.Debug("Syslog listen addr resolved to %", udpAddr)
 
 	conn, err := net.ListenUDP(s.net, udpAddr)
-	s.conn = conn
-	s.cfg.Log.Debug("Syslog listen started %v", conn)
+
 	if err != nil {
 		return err
 	}
 
-	s.cfg.Log.Debug("Syslog add WG %v", conn)
+	s.conn = conn
+	s.cfg.Log.Info("Syslog rcv service started on %s://%s", s.net, s.addr)
 
 	go func(s *Syslog) {
 		defer s.conn.Close()
 		defer s.cfg.Wg.Done()
-		rcvBuff := make([]byte, 1400)
-		s.cfg.Log.Debug("Syslog buffer created %s", "")
+		rcvBuff := make([]byte, 2048)
+		s.cfg.Log.Debug("Syslog buffer[2048] created %s")
 
 		for {
 			n, _, err := s.conn.ReadFromUDP(rcvBuff)
 
 			if err != nil {
-				s.cfg.Log.Error("Read from %s failed: %s", err, s.addr)
+				s.cfg.Log.Error("Syslog read from %s failed with: %s", s.addr, err)
 			} else {
 				s.cfg.Log.Info("%d - %s ", n, rcvBuff[0:n])
 			}
